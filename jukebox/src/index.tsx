333be57b8da2e00fba237fd467bab2435db54189
kasparpg <43644986+kasparpg@users.noreply.github.com>
2020-09-06 21:43:40 +0200

    navbar

--- jukebox/src/index.tsx
+++ jukebox/src/index.tsx
@@ -6,11 +6,45 @@ import * as serviceWorker from './serviceWorker';
 
 var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        <a href="index.tsx" id="home">
+          JukeBox
+        </a>
+        <a href="index.tsx" className="navbuttons">
+          Home
+        </a>
+        <a href="index.tsx" className="navbuttons">
+          Songs
+        </a>
+        <a href="index.tsx" className="navbuttons">
+          Documentation
+        </a>
+      </div>
+    );
+  }
+  
+}
 
-ReactDOM.render(
-  element,
+class Main extends React.Component {
+  render() {
+    return(
+      <div id="main">
+
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Main />,
   document.getElementById('root')
 );
+
+ReactDOM.render(<NavBar />,
+  document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
