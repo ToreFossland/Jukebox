84d44a38dad4d5e77dd212ea7dddf68f5bfbe485
kasparpg <43644986+kasparpg@users.noreply.github.com>
2020-09-07 14:25:25 +0200

    Folder Structure updated

--- jukebox/src/components/songs.tsx
+++ jukebox/src/components/songs.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/layout.css';
+import '../resources/styling/layout.css';
+import '../resources/styling/songlayout.css';
 
 class Songs extends React.Component {
     render() {
       return(
         <div id="main">
             <h1>Songs</h1>
+            <div className="songlist">
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+                
+            </div>
         </div>
       );
     }
