f760f6f18268c04f77cce712946d1efdea0f6a3a
kasparpg <43644986+kasparpg@users.noreply.github.com>
2020-09-07 22:47:23 +0200

    Footer + Switch

--- jukebox/src/App.tsx
+++ jukebox/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from "./components/navbar"
-import Home from "./components/home"
-import Songs from "./components/songs"
-import Info from "./components/info"
+import NavBar from "./components/navbar";
+import Home from "./components/home";
+import Songs from "./components/songs";
+import Info from "./components/info";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Songs />
         <Info />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ jukebox/src/components/footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../resources/styling/layout.css';
+
+class Footer extends React.Component {
+    render() {
+      return(
+        <div id="footermain">
+            <h2>Created By:</h2>
+            <p>Haakon, Kaspar og Tore.</p>
+            <label className="darkswitch">
+                <input type="checkbox"></input>
+                <span className="slider round">
+                </span>
+            </label>
+        </div>
+      );
+    }
+  }
+
+  export default Footer;
