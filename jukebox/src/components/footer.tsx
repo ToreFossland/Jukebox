38c2e116e06aad4d45d72c9b7739736daa187e85
kasparpg <43644986+kasparpg@users.noreply.github.com>
2020-09-10 12:35:18 +0200

    Update footer.tsx

--- jukebox/src/components/footer.tsx
+++ jukebox/src/components/footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../resources/styling/layout.css';
 
+
 class Footer extends React.Component {
     render() {
       return(
@@ -9,7 +10,7 @@ class Footer extends React.Component {
             <h2>Created By:</h2>
             <p>Haakon, Kaspar og Tore.</p>
             <label className="darkswitch">
-                <input type="checkbox"></input>
+                <input type="checkbox" id="switch"></input>
                 <span className="slider round">
                 </span>
             </label>
