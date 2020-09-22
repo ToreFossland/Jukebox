4cec2531520e5db404904780d3f6b89f4d6c203d
kasparpg <43644986+kasparpg@users.noreply.github.com>
2020-09-22 14:00:45 +0200

    footer with dark theme

--- jukebox/src/components/footer.tsx
+++ jukebox/src/components/footer.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import '../resources/styling/layout.css';
 import {Context} from '../context'
 
+const Footer = ({...props}) => {
 
-const Footer = () => {
+  const handleChange = () => {
+    props.onChange();
+  }
       return(
         <div id="footermain">
             <h2>Created By:</h2>
             <p>Haakon, Kaspar og Tore.</p>
+            <button id="pageThemeButton" onClick={handleChange}>Change Theme</button>
         </div>
       );
   }
