46f2ac6e981b4de9ef1ff9f370f5618b6e94ee8b
kasparpg <43644986+kasparpg@users.noreply.github.com>
2020-09-28 12:31:23 +0200

    #12

--- jukebox/src/components/info.tsx
+++ jukebox/src/components/info.tsx
@@ -11,7 +11,7 @@ const Info = ({...props})=>{
       return(
         <div id="infomain" style={{background: props.btheme, color: props.ctheme}}>
             <h1>Info</h1>
-            <p> Press Dua Lipa's head, torso or tonga to change their color!</p>
+            <p> The different dancers have different interactive effects; try clicking or hovering on different parts to see what they can do! </p>
         </div>
       );
     }
